6cb43ca3cfe907f88654b1eda2571eebb7fb67c2
Adam Chandra Setianugraha <adamchandra890@gmail.com>
2025-06-10 13:28:49 +0700

    router

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1 class="text-red">Index page</h1>
+        <NuxtLink to="/blog/hello-world">
+            Go to blog post
+        </NuxtLink>
+    </div>
+</template>
