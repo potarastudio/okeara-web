71c8722106f8670d6b08e75ea8ab14513fb9a27f
potarastudio <hi@potarastudio.com>
2025-08-22 13:44:41 +0700

    Development (#31)
    * slicing page
    
    * faq
    
    * fix faq
    
    * fix shop
    
    * fix responsive
    
    * fix auto layout home page
    
    * implement parallax effect
    
    * fix auto layout our water, our club, our partner, and faq
    
    * remove smooth scroll
    
    * fix zoom
    
    * create page research and certificate
    
    * fix marque
    
    * change asset
    
    * change asset okeara blue
    
    * fix add on
    
    * fix parallax
    
    * add 3d
    
    * fix version three
    
    * change lib 3d
    
    * fix bugs responsive
    
    * fix feedback
    
    * fetch email to google shet
    
    * fix feedback
    
    * create page terms and condition
    
    * fix GLBViewer
    
    * detail shop
    
    * add docker file
    
    * change port
    
    * test
    
    * implement switch country
    
    * fix update
    
    * fix return null
    
    ---------
    
    Co-authored-by: Adam Chandra Setianugraha <adamchandra890@gmail.com>

--- layout/DefaultLayout.vue
+++ layout/DefaultLayout.vue
@@ -14,12 +14,13 @@ import FooterComponents from '~/components/Footer/FooterComponents.vue'
 import ModalRegion from '~/components/Modal/ModalRegion.vue'
 
 const isModalOpen = ref(false)
-const companyId = ref<number | null>(null)
+const companyId = ref<number | null>(3)
+const companyCode = ref<string | null>("ID")
 
 provide('isModalOpen', isModalOpen)
 provide('openModal', () => (isModalOpen.value = true))
 provide('closeModal', () => (isModalOpen.value = false))
 provide('companyId', companyId)
-
+provide('companyCode', companyCode)
 
 </script>
